5857870dd15649c7be78c602d0a44c7394dd87d6
HHARSANI <61774014+HARSHHARSANI@users.noreply.github.com>
2024-03-10 13:46:12 +0530

    code

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Contact = () => {
   return (
     <div className="text-4xl pt-8 w-full mr-2 ">
-      <div className="w-auto pl-2 bg-white mx-10 rounded-xl  h-36  bg-purple-300">
+      <div className="w-auto pl-2  mx-10 rounded-xl  h-36  bg-purple-300">
         <div className="mt-4 ml-4  ">
           <button className=" bg-white px-3  py-1  rounded-xl text-base mt-6 ml-4">
             Contact
